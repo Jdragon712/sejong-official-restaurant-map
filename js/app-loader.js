029461f3ea20ad8587e21502935357a1268a99a4
Jdragon712 <pjy712@gmail.com>
2026-07-05 11:47:40 +0900

    Hotfix: app-loader.js via jsdelivr until app.js deploy restored (v20260717)

---
+++ js/app-loader.js
@@ -0,0 +1,2 @@
+/** Temporary loader: main branch app.js deploy fix in progress */
+import "https://cdn.jsdelivr.net/gh/Jdragon712/sejong-official-restaurant-map@59a4ad9f4be45d7710f4009c58426aef03435fe5/js/app.js";
