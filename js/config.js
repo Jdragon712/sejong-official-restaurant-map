95fa99c1120fb36b9c2e8ffc9c110f0b9f191da3
JDragon712 <pjy712@gmail.com>
2026-07-07 01:41:02 +0900

    Deploy web/ via API (v20260727)

--- js/config.js
+++ js/config.js
@@ -2,4 +2,9 @@
 export const MAP_CONFIG = {
   kakaoJsKey: "f7481d73aba254de26514c2ce0fd24db",
   preferKakaoMap: true,
+
+  /** Naver Client ID (use ?ncpKeyId=... for the JS SDK, per https://navermaps.github.io/maps.js.ncp/docs/tutorial-2-Getting-Started.html) */
+  naverClientId: "0rhl42vxm3",
+  /** true면 네이버맵 우선 사용 (Kakao보다 우선) */
+  preferNaverMap: true,
 };
